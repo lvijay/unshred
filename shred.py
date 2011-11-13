be4c93881d37b1be5e90ae8ee81708a6f9db766f
Vijay Lakshminarayanan <laksvij@gmail.com>
2011-11-13 21:53:42 +0530

    defined main, working version

--- shred.py
+++ shred.py
@@ -8,14 +8,14 @@
 import sys
 from PIL import Image
 from random import shuffle
-from math import abs
 
-def shred(img, width):
-    result = Image.new(image.mode, image.size)
+def shred(img, strip_width):
+    width, height = img.size
+    result = Image.new(img.mode, img.size)
     num_cols = width / strip_width # assume integer
 
     cs = strip_width
-    cols = [src.crop((i*cs, 0, (i+1)*cs, height)) for i in xrange(num_cols)]
+    cols = [img.crop((i*cs, 0, (i+1)*cs, height)) for i in xrange(num_cols)]
 
     array = []
     while True:
@@ -31,26 +31,29 @@ def shred(img, width):
         break
 
     for i, k in enumerate(array):
-        result.paste(cols[k], (i*width, 0))
+        result.paste(cols[k], (i*strip_width, 0))
 
     return result
 
-if __name__ == '__main__':
+def main(argv):
     try:
-        filename = sys.argv[1]
-        saveto = sys.argv[2]
-        strip_width = 32 if len(sys.argv) < 3 else int(sys.argv[3])
+        filename = argv[1]
+        saveto = argv[2]
+        width = int(argv[3])
     except IndexError:
-        print >> sys.stderr, ('Usage: %s [source] [dest] [width=32]' % sys.argv[0])
+        print >> sys.stderr, ('Usage: %s [source] [dest] [width=32]' % argv[0])
         exit(1)
 
     try:
         image = Image.open(filename)
-        shredded_image = shred(image, strip_width)
+        shredded_image = shred(image, width)
 
         shredded_image.save(saveto)
     except IOError, e:
         print >> sys.stderr, e
         exit(2)
+
+if __name__ == '__main__':
+    main(sys.argv)
 
 # shred.py ends here
