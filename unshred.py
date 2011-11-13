565148ccef7dc4a0ad20398f7f19eab6eb9b8aa6
Vijay Lakshminarayanan <laksvij@gmail.com>
2011-11-13 13:49:45 +0530

    step 1 complete

--- unshred.py
+++ unshred.py
@@ -13,9 +13,19 @@ def unshred(src):
     '''unshred(shredded_img) -> unshredded_img'''
     result = Image.new(image.mode, image.size)
 
-    ## step 1, find column boundaries
+    width, height = image.size
+    col_size = 32               # assume
+
+    ## step 1, find columns
+    num_cols = width / col_size # assume integer
+
+    cs = col_size
+    strips = [src.crop((i*cs, 0, (i+1)*cs, height)) for i in xrange(num_cols)]
+
     ## step 2, find matching columns
     ## step 3, merge columns
+    for i, strip in enumerate(strips):
+        result.paste(strip, (i*col_size, 0))
 
     return result
 
@@ -26,7 +36,7 @@ if __name__ == '__main__':
 
         image = Image.open(filename)
         unshredded_image = unshred(image)
-        unshredded_image.save(saveto, 'png')
+        unshredded_image.save(saveto)
     except IndexError:
         print >> sys.stderr, ('Usage: %s [source_image] [dest_image]'
                               % sys.argv[0])
