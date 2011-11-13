056e8f58bf48a5679c6d3ab0f6581222f55ebfb8
Vijay Lakshminarayanan <laksvij@gmail.com>
2011-11-13 13:02:26 +0530

    interfaces defined

--- unshred.py
+++ unshred.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 ## unshred - form original image from an image uniformly shredded into
 ##           columns
@@ -8,19 +9,26 @@
 import sys
 from PIL import Image
 
-def unshred(src, dest):
-    # just return the source for now
-    return src
+def unshred(src):
+    # for now, just create a new image and return
+    result = Image.new(image.mode, image.size)
+    return result
 
 if __name__ == '__main__':
     try:
-        filename = sys.argv[0]
-        saveto = sys.argv[1]
+        filename = sys.argv[1]
+        saveto = sys.argv[2]
 
         image = Image.open(filename)
-        unshredded_image = Image.new(image.mode, image.size)
-        unshred(image, unshredded_image)
+        unshredded_image = unshred(image)
         unshredded_image.save(saveto, 'png')
+    except IndexError:
+        print >> sys.stderr, ('Usage: %s [source_image] [dest_image]'
+                              % sys.argv[0])
+        exit(1)
+    except IOError, e:
+        print >> sys.stderr, e
+        exit(2)
     finally:
         pass
 
