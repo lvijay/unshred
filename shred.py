777d2628863f07b4b8303ef95086d3205b35d2fb
Vijay Lakshminarayanan <laksvij@gmail.com>
2011-11-14 09:06:06 +0530

    added usage instructions and license

--- shred.py
+++ shred.py
@@ -1,9 +1,41 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-## shred - shred a given image into columns of equal width strips
+## shred.py - shred a given image into columns of equal width strips.
+##
+##            Takes some effort to ensure that adjacent strips don't
+##            end up next to each other.
+##
+## Usage:
+##
+##    ./shred.py source destination [strip_width=32]
+##
+##    source - the input file.  Must exist.
+##    destination - the destination file.  Will be created or
+##                  overwritten if exists.
+##
+##    strip_width - the width of each column.  Taken as 32 if not
+##                  provided.
+##
+##                  It is the users responsibility to ensure that the
+##                  source.size.width is a multiple of strip_width.
 ##
 ## Copyright © 2011, Vijay Lakshminarayanan <laksvij@gmail.com>
+##
+## This program is free software; you can redistribute it and/or
+## modify it under the terms of the GNU General Public License as
+## published by the Free Software Foundation; either version 3, or (at
+## your option) any later version.
+##
+## This program is distributed in the hope that it will be useful, but
+## WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+## General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with GNU Emacs; see the file COPYING. If not, write to the
+## Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+## Boston, MA 02110-1301, USA.
 
 import sys
 from PIL import Image
